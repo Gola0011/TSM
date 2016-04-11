efc7151576ac9282e4f99d0411eaf26a8fb41541
Matteo <matteobtt@users.noreply.github.com>
2016-04-11 16:28:28 +0200

    Menu laterale

--- js/javascript.js
+++ js/javascript.js
@@ -1,42 +1,45 @@
+var categoriaCorrente = "";
+
 $(function() {
-    richiedi(1);
+    richiedi(1, categoriaCorrente);
 });
 
-function richiedi(pagina) {
+function richiedi(pagina, categoria) {
     $("#evento").empty();
     var offset = 8 * (pagina - 1);
+    categoriaCorrente = categoria;
     $.ajax({
         type: "POST",
         url:  "http://incaneva.it/wp-admin/admin-ajax.php",
-        data: "blog=1,6,7,8&action=incaneva_events&limit=8&old=true&offset=" + offset,
+        data: "blog=1,6,7,8&action=incaneva_events&limit=8&old=true&offset=" + offset + categoria,
         dataType: "json",
         success: function(risposta) {
             $.each(risposta.data, function (key, value) {
                 var colore = "panel-default";
                 switch (value.event_type[1]) {
-                    case "storia-cultura":
-                        colore = "panel-danger";
-                        break;
-                    case "passioni":
-                        colore = "panel-warning";
-                        break;
-                    case "natura":
-                        colore = "panel-success";
-                        break;
-                    case "sport":
-                        colore = "panel-info";
-                        break;
-                    case "enogastronomia":
-                        colore = "panel-warning";
-                        break;
-                    default:
-                        colore = "panel-default";
-                        break;
+                case "storia-cultura":
+                    colore = "panel-danger";
+                    break;
+                case "passioni":
+                    colore = "panel-info";
+                    break;
+                case "natura":
+                    colore = "panel-success";
+                    break;
+                case "sport":
+                    colore = "panel-primary";
+                    break;
+                case "enogastronomia":
+                    colore = "panel-warning";
+                    break;
+                default:
+                    colore = "panel-default";
+                    break;
                 }
                 var dataInizio = moment.unix(value.evcal_srow).locale('it').format('LLLL');
                 $("#evento").append('<div class="col-sm-6 col-md-3" >' +
                     '<div class="panel ' + colore + '">' +
-                    '   <div class="panel-heading">' + value.event_type[1] + '</div>' +
+                    '   <div class="panel-heading text-center">' + value.event_type + '</div>' +
                     '   <div class="panel-body">' +
                     '       <img src="' + value.post_thumbnail + '" class="img-responsive" alt="' + value.post_title + '">' +
                     '       <h3>' + value.post_title + '</h3>' +
