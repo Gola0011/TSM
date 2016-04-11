4d5f6c807ccb0e8121fb32d48654d164770944dd
Matteo <matteobtt@users.noreply.github.com>
2016-04-11 15:20:33 +0200

    Aggiunti colori

--- js/javascript.js
+++ js/javascript.js
@@ -12,15 +12,37 @@ function richiedi(pagina) {
         dataType: "json",
         success: function(risposta) {
             $.each(risposta.data, function (key, value) {
+                var colore = "panel-default";
+                switch (value.event_type[1]) {
+                    case "storia-cultura":
+                        colore = "panel-danger";
+                        break;
+                    case "passioni":
+                        colore = "panel-warning";
+                        break;
+                    case "natura":
+                        colore = "panel-success";
+                        break;
+                    case "sport":
+                        colore = "panel-info";
+                        break;
+                    case "enogastronomia":
+                        colore = "panel-warning";
+                        break;
+                    default:
+                        colore = "panel-default";
+                        break;
+                }
                 var dataInizio = moment.unix(value.evcal_srow).locale('it').format('LLLL');
                 $("#evento").append('<div class="col-sm-6 col-md-3" >' +
-                    '   <div class="thumbnail">' +
+                    '<div class="panel ' + colore + '">' +
+                    '   <div class="panel-heading">' + value.event_type[1] + '</div>' +
+                    '   <div class="panel-body">' +
                     '       <img src="' + value.post_thumbnail + '" class="img-responsive" alt="' + value.post_title + '">' +
-                    '       <div class="caption">' +
-                    '           <h3>' + value.post_title + '</h3>' +
-                    '           <p>' + value.post_excerpt + '</p>' +
-                    '           <footer class="text-muted"><small>' + '<i class="fa fa-calendar"></i> ' + dataInizio + '</small></footer>' +
-                    '       </div>' +
+                    '       <h3>' + value.post_title + '</h3>' +
+                    '       <p>' + value.post_excerpt + '</p>' +
+                    '       <footer class="text-muted"><small>' + '<i class="fa fa-calendar"></i> ' + dataInizio + '</small></footer>' +
+                    '   </div>' +
                     '</div>');
             })
         },
